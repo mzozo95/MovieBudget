2ffcd2109499ed3c649541e9e34f0ce47804805c
Zoltan Majlath <majlath.tech@gmail.com>
2021-02-24 23:00:04 +0100

    Add pure navhostController setup

--- app/src/main/java/com/majlathtech/moviebudget/ui/main/MainActivityNavHost.java
+++ app/src/main/java/com/majlathtech/moviebudget/ui/main/MainActivityNavHost.java
@@ -3,8 +3,12 @@ package com.majlathtech.moviebudget.ui.main;
 import android.os.Bundle;
 
 import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.majlathtech.moviebudget.R;
@@ -14,23 +18,25 @@ public class MainActivityNavHost extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainnavhost);
-
-        BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(menuItem -> {
-            switch (menuItem.getItemId()) {
-                case R.id.search:
-                    Navigation.findNavController(MainActivityNavHost.this, R.id.navhostFragment).navigate(R.id.action_to_search);
-
-                    return true;
-                case R.id.favorites:
-                    Navigation.findNavController(MainActivityNavHost.this, R.id.navhostFragment).navigate(R.id.action_to_favorites);
-                    return true;
-            }
-            return false;
-        });
+        setupNavigation();
     }
 
-    public void navigate(@IdRes int resId) {
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        setupNavigation();
+    }
+
+    private void setupNavigation() {
+        //https://developer.android.com/guide/navigation/navigation-ui#bottom_navigation
+        //https://developer.android.com/guide/navigation/navigation-ui#Tie-navdrawer
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navhostFragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+    private void navigate(@IdRes int resId) {
         //https://stackoverflow.com/a/63625046
         //https://issuetracker.google.com/issues/142847973
         //https://stackoverflow.com/a/51974492
